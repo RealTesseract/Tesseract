f60ba6b1d7c4f28762e72af6b0a4fe211b344144
Apple <ytapplegamer@gmail.com>
2016-11-13 11:15:28 -0500

    New particles, forgot to add them earlier!
    from PMMP!

--- src/pocketmine/level/particle/BlockForceFieldParticle.php
+++ src/pocketmine/level/particle/BlockForceFieldParticle.php
@@ -1,4 +1,4 @@
-<?php
++<?php
 
 /*
  *
@@ -18,13 +18,13 @@
  *
  *
 */
-
-namespace pocketmine\level\particle;
-
-use pocketmine\math\Vector3;
-
-class LargeExplodeParticle extends GenericParticle{
-	public function __construct(Vector3 $pos){
-		parent::__construct($pos, Particle::TYPE_LARGE_EXPLODE);
-	}
+ 
+ namespace pocketmine\level\particle;
+ 
+ use pocketmine\math\Vector3;
+ 
+ class BlockForceFieldParticle extends GenericParticle{
+ 	public function __construct(Vector3 $pos, int $data = 0){
+ 		parent::__construct($pos, Particle::TYPE_BLOCK_FORCE_FIELD, $data); //TODO: proper encode/decode of data
+ }
 }
