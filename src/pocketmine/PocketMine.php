618ca8ad527ef9e197dc520d66ad6c2c717e8346
RateekMCPE <rateek34@gmail.com>
2016-11-22 22:22:12 +0100

    Update lol

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,8 +75,8 @@ namespace pocketmine {
 	const VERSION = ""; //will be set by CI to a git hash
 	const API_VERSION = "2.1.0";
 	const CODENAME = "Crystal";
-	const MINECRAFT_VERSION = "v0.16.1 alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.16.0.5";
+	const MINECRAFT_VERSION = "v0.16.2 alpha";
+	const MINECRAFT_VERSION_NETWORK = "0.16.2";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
