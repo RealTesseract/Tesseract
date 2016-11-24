d45136e23e1c627f1c0b09e67fab60aca8fadc3b
IISweepeDII <sweeps.gh@gmail.com>
2016-11-24 16:23:44 -0500

    Chickens now drop Feathers + Chicken
    The coding had a \ at first, but after testing, it still did not work. I added this new code in, and this works :)

--- src/pocketmine/entity/Chicken.php
+++ src/pocketmine/entity/Chicken.php
@@ -57,21 +57,8 @@ class Chicken extends Animal{
 	}
 	
 	public function getDrops(){
-		$drops = [];
-		if ($this->lastDamageCause instanceof EntityDamageByEntityEvent and $this->lastDamageCause->getEntity() instanceof Player) {
-			
-				switch (\mt_rand(0, 2)) {
-					case 0:
-						$drops[] = ItemItem::get(ItemItem::RAW_CHICKEN, 0, 1);
-						break;
-					case 1:
-						$drops[] = ItemItem::get(ItemItem::FEATHER, 0, 1);
-						break;
-					case 2:
-						$drops[] = ItemItem::get(ItemItem::FEATHER, 0, 2);
-						break;
-				}
-		}
+		$drops = array(ItemItem::get(ItemItem::RAW_CHICKEN, 0, mt_rand(1, 2)));
+		$drops[] = ItemItem::get(ItemItem::FEATHER, 0, mt_rand(1, 2));
 		return $drops;
 	}
 }
