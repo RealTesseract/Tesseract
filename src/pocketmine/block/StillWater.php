fb73acc60e3cdc1077fec31b6545030757bfc9dd
IISweepeDII <sweeps.gh@gmail.com>
2016-12-11 15:47:13 -0500

    No scheduling on still water
    dktapp commit

--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -26,6 +26,13 @@ use pocketmine\level\Level;
 class StillWater extends Water{
 
 	protected $id = self::STILL_WATER;
+	
+	public function onUpdate($type){
+ 		if($type !== Level::BLOCK_UPDATE_SCHEDULED){
+ 			return parent::onUpdate($type);
+ 		}
+ 		return false;
+ 	}
 
 	public function onUpdate($type){
 		if($type == Level::BLOCK_UPDATE_NORMAL){
