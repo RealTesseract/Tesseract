51570fd2c7da46c412bea6b7c94cad9dcea664ba
Apple <ytapplegamer@gmail.com>
2016-12-18 03:31:57 -0500

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ for file in **/*.php; do
 done
 echo Lint done successfully.
 echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
-if ls plugins/Tesseract/Tesseract*.phar >/dev/null 2>&1; then
+if ls plugins/DevTools_OUTPUT/Tesseract*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
     echo No phar created!
