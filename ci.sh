17b5dd401f1b69d3d5141d77195d198f48429feb
Apple <ytapplegamer@gmail.com>
2016-12-18 03:10:48 -0500

    Fix our travis

--- ci.sh
+++ ci.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir plugins
+curl -fsSL https://github.com/TesseractTeam/DevTools/releases/download/1/DevTools.phar
 echo Running lint...
 shopt -s globstar
 for file in **/*.php; do
@@ -6,7 +8,7 @@ for file in **/*.php; do
     [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
 echo Lint done successfully.
-echo -e "version\nmakephar\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
+echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
 if ls plugins/Tesseract/Tesseract*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
