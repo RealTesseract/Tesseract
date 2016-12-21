279d397f80f100b086a7acb820ab39e1c32afa37
Apple <ytapplegamer@gmail.com>
2016-12-20 19:37:18 -0500

    Update Info.php

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -27,7 +27,8 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 100;
-	const ACCEPTED_PROTOCOLS = [100];
+	const MINECRAFT_VERSION = "v1.0.0 alpha";
+	const MINECRAFT_VERSION_NETWORK = "1.0.0";
 	
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
