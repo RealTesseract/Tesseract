37d8a266725fb127a02ae0cee9c52e9e6fa42b89
Nought57 <SkyDrifter2000@gmail.com>
2016-12-28 04:22:17 +0900

    CodeUpdate (#78)
    Update Public License, author and link.

--- src/pocketmine/block/PurpurBlock.php
+++ src/pocketmine/block/PurpurBlock.php
@@ -2,24 +2,24 @@
 
 /*
  *
- *  _______                                _   
- * |__   __|                              | |  
- *    | | ___  ___ ___  ___ _ __ __ _  ___| |_ 
- *    | |/ _ \/ __/ __|/ _ \ '__/ _` |/ __| __|
- *    | |  __/\__ \__ \  __/ | | (_| | (__| |_ 
- *    |_|\___||___/___/\___|_|  \__,_|\___|\__|
- *                                             
+ *    _______                                _
+ *   |__   __|                              | |
+ *      | | ___  ___ ___  ___ _ __ __ _  ___| |_
+ *      | |/ _ \/ __/ __|/ _ \  __/ _` |/ __| __|
+ *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
+ *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
+ *
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * @author Tessetact Team
+ * @link http://www.github.com/TesseractTeam/Tesseract
  * 
  *
-*/
+ */
 
 namespace pocketmine\block;
 
--- src/pocketmine/network/protocol/CameraPacket.php
+++ src/pocketmine/network/protocol/CameraPacket.php
@@ -9,12 +9,14 @@
  *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
  *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
  *
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * @author Tessetact Team
+ * @link http://www.github.com/TesseractTeam/Tesseract
  * 
  *
  */
