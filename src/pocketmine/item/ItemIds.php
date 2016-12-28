9b711ad27b47bf8ca96c27da452bc93b255158f5
remote_vase <web.articlerequests@gmail.com>
2016-12-28 11:41:07 -0500

    Update ItemIds.php

--- src/pocketmine/item/ItemIds.php
+++ src/pocketmine/item/ItemIds.php
@@ -209,7 +209,7 @@
   //const POPPED_CHORUS_FRUIT = 433;
   //const DRAGONS_BREATH = 437;
 	const SPLASH_POTION = 438;
-	
+	const ELYTRA = 444;
 	const BEETROOT = 457;
 	const BEETROOT_SEEDS = 458; const BEETROOT_SEED = 458;
 	const BEETROOT_SOUP = 459;
