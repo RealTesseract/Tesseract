bcc7839476702bcadca6d502d5c21599e5228bbc
remote_vase <web.articlerequests@gmail.com>
2016-12-27 20:38:39 -0500

    Update Elytra.php

--- src/pocketmine/item/Elytra.php
+++ src/pocketmine/item/Elytra.php
@@ -8,7 +8,7 @@ use pocketmine\Player;
 
 use pocketmine\item\Armor;
 
-use pocketmine\item\elytra;
+# use pocketmine\item\elytra; NOTE: Should I seperate it?
 
 class Elytra extends Armor {
 
@@ -17,5 +17,19 @@ public function __construct($meta = 0, $count = 1){
   parent::__construct(444, $meta, $count, "Elytra");
 	
   }
-
+	public function getArmorTier(){
+		return false;
+	}
+	public function getArmorType(){
+		return Armor::TYPE_CHESTPLATE;
+	}
+	public function getMaxDurability(){
+		return false; //SOMEONE HELP HERE! IDK WHAT TO DO!
+	}
+	public function getArmorValue(){
+		return 0;
+	}
+	public function isChestplate(){
+		return true;
+	}
 }
