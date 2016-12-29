1f70526bda13dd6093d92a30e2e5b86cab358a66
Nought57 <SkyDrifter2000@gmail.com>
2016-12-29 20:07:55 +0900

    Update Server.php
    delete pocketmine.yml

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1732,10 +1732,11 @@ class Server{
 §e#  §cPackage: §d$package
 §e#################################################################");
 
-			if(!file_exists($this->dataPath . "pocketmine.yml")){
+			//not using (create empty pocketmine.yml??)
+			/*if(!file_exists($this->dataPath . "pocketmine.yml")){
 				$content = file_get_contents($this->filePath . "src/pocketmine/resources/pocketmine.yml");
 				@file_put_contents($this->dataPath . "pocketmine.yml", $content);
-			}
+			}*/
 			$nowLang = $this->getProperty("settings.language", "eng");
 
 			//Crashes unsupported builds without the correct configuration
