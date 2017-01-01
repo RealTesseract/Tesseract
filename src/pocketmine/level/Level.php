63feffb3485375eafc75c9e71f97f77674866055
Nought57 <SkyDrifter2000@gmail.com>
2017-01-01 20:57:43 +0900

    Update Level.php
    Update Level.php file.

--- src/pocketmine/level/Level.php
+++ src/pocketmine/level/Level.php
@@ -512,8 +512,7 @@ class Level implements ChunkManager, Metadatable{
 
 	public function close(){
 
-		if($this->getAutoSave()){
-			$this->save();
+		if($this->getAutoSave()) $this->save();
 		}
 
 		foreach($this->chunks as $chunk){
