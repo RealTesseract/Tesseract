59c6d656637d2e569ab84069994360872c548cf0
Nought57 <SkyDrifter2000@gmail.com>
2017-01-12 14:40:24 +0900

    Update Elytra.php
    Add getMaxStackSize.

--- src/pocketmine/item/Elytra.php
+++ src/pocketmine/item/Elytra.php
@@ -5,17 +5,22 @@ use pocketmine\item\Item;
 use pocketmine\item\Armor;
 class Elytra extends Armor {
 
-public function __construct($meta = 0, $count = 1){
+    public function __construct($meta = 0, $count = 1){
+        parent::__construct(self::ELYTRA, $meta, $count, "Elytra Wings");
+	}
 	
-  parent::__construct(self::ELYTRA, $meta, $count, "Elytra Wings");
-	
-  }
+	public function getMaxStackSize() : int {
+        return 1;
+    }
+
 	public function getArmorType(){
 		return Armor::TYPE_CHESTPLATE;
 	}
+	
 	public function getMaxDurability(){
 		return 431;
 	}
+	
 	public function isChestplate(){
 		return true;
 	}
