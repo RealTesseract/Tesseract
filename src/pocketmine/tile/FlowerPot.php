db2e17b7a937265ba773270401124d192ea13d05
Apple <ytapplegamer@gmail.com>
2017-01-11 22:24:49 -0500

    Keeping up with genisys https://github.com/iTXTech/Genisys/commit/0838b68354bdc302d1589eeff376a3f01a524665

--- src/pocketmine/tile/FlowerPot.php
+++ src/pocketmine/tile/FlowerPot.php
@@ -19,14 +19,9 @@
  *
  */
 
-/*
- * Originally by @beito123
- * https://github.com/beito123/PocketMine-MP-Plugins/blob/master/test%2FFlowerPot%2Fsrc%2Fbeito%2FFlowerPot%2Fomake%2FSkull.php
- */
-
 namespace pocketmine\tile;
 
-use pocketmine\block\Block;
+use pocketmine\item\Item;
 use pocketmine\level\format\Chunk;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\nbt\tag\IntTag;
@@ -36,28 +31,23 @@ use pocketmine\nbt\tag\StringTag;
 class FlowerPot extends Spawnable{
 
 	public function __construct(Chunk $chunk, CompoundTag $nbt){
-        parent::__construct($chunk, $nbt);
-		if(!isset($nbt->item)){
+		if(!isset($nbt->item) or !($nbt->item instanceof ShortTag)){
 			$nbt->item = new ShortTag("item", 0);
 		}
-		if(!isset($nbt->mData)){
+		if(!isset($nbt->mData) or !($nbt->mData instanceof IntTag)){
 			$nbt->mData = new IntTag("mData", 0);
 		}
+		parent::__construct($chunk, $nbt);
 	}
 
-	public function getFlowerPotItem(){
-		return $this->namedtag["item"];
+	public function getItem() : Item{
+		return Item::get((int) ($this->namedtag["item"] ?? 0), (int) ($this->namedtag["mData"] ?? 0));
 	}
 
-	public function getFlowerPotData(){
-		return $this->namedtag["mData"];
-	}
-
-	public function setFlowerPotData($item, $data){
-		$this->namedtag->item = new ShortTag("item", (int) $item);
-		$this->namedtag->mData = new IntTag("mData", (int) $data);
-        $this->onChanged();
-		return true;
+	public function setItem(Item $item){
+		$this->namedtag["item"] = $item->getId();
+		$this->namedtag["mData"] = $item->getDamage();
+		$this->onChanged();
 	}
 
 	public function getSpawnCompound(){
