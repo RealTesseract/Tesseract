5040cdfc3f476ca549ed3d25c2663e4267a84e91
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2017-01-16 13:01:29 -0500

    Update Arrow.php

--- src/pocketmine/item/Arrow.php
+++ src/pocketmine/item/Arrow.php
@@ -23,6 +23,6 @@ namespace pocketmine\item;
 
 class Arrow extends Item{
 	public function __construct($meta = 0, $count = 1) {
-		Parent::__construct(self::ARROW, (int) $meta, $count, "Arrow");
+		parent::__construct(self::ARROW, (int) $meta, $count, "Arrow");
 	}
 }
