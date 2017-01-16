66505b8e07a6669cfc789171237e843f5bf99768
Andre | xZeroMCPE <andrethegamer911@gmail.com>
2017-01-16 12:58:54 -0500

    Fixed arrow bug
    Should fixed it i think, it's how i see other repo(s) does it...

--- src/pocketmine/item/Arrow.php
+++ src/pocketmine/item/Arrow.php
@@ -23,6 +23,6 @@ namespace pocketmine\item;
 
 class Arrow extends Item{
 	public function __construct($meta = 0, $count = 1) {
-		parent::__construct(self::ARROW, $meta, $count, "Arrow");
+		Parent::__construct(self::ARROW, (int) $meta, $count, "Arrow");
 	}
 }
