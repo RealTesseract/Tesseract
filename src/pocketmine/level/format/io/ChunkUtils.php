2c3144b5a38a0a5407f6fd629b11ed20d1c8421c
Apple <ytapplegamer@gmail.com>
2017-01-18 20:16:39 -0500

    Some anvil improvements

--- src/pocketmine/level/format/io/ChunkUtils.php
+++ src/pocketmine/level/format/io/ChunkUtils.php
@@ -34,16 +34,21 @@
  	 */
 	public static final function reorderByteArray(string $array) : string{
 		$result = str_repeat("\x00", 4096);
- 		$i = 0;
- 		for($x = 0; $x < 16; ++$x){
- 			for($z = 0; $z < 256; $z += 16){
- 				$zx = ($z + $x);
- 				for($y = 0; $y < 4096; $y += 256){
- 					$result{$i} = $array{$y + $zx};
- 					++$i;
- 				}
+ 		if($array !== $result){
+			$i = 0;
+			$zM = 0;
+			$yM = 0;
+			for($x = 0; $x < 16; ++$x){
+				$zM = $x + 256;
+				for($z = $x; $z < $zM; $z += 16){
+					$yM = $z + 4096;
+					for($y = $z; $y < $yM; $y += 256){
+ 						$result{$i} = $array{$y};
+						++$i;
+					}
  			}
- 		}
+ 			}
+		}
  		return $result;
  	}
  
@@ -54,26 +59,33 @@
  	 *
  	 * @return string length 2048
  	 */
- 	public static final function reorderNibbleArray(string $array) : string{
- 		$result = str_repeat("\x00", 2048);
- 		$i = 0;
- 		for($x = 0; $x < 8; ++$x){
- 			for($z = 0; $z < 16; ++$z){
- 				$zx = (($z << 3) | $x);
- 				for($y = 0; $y < 8; ++$y){
- 					$j = (($y << 8) | $zx);
- 					$i1 = ord($array{$j});
- 					$i2 = ord($array{$j | 0x80});
- 					$result{$i}        = chr(($i2 << 4) | ($i1 & 0x0f));
- 					$result{$i | 0x80} = chr(($i1 >> 4) | ($i2 & 0xf0));
- 					$i++;
- 				}
+ 	public static final function reorderNibbleArray(string $array, string $commonValue = "\x00") : string{
+ 		$result = str_repeat($commonValue, 2048);
+		if($array !== $result){
+			$i = 0;
+			for($x = 0; $x < 8; ++$x){
+				for($z = 0; $z < 16; ++$z){
+					$zx = (($z << 3) | $x);
+					for($y = 0; $y < 8; ++$y){
+						$j = (($y << 8) | $zx);
+						$j80 = ($j | 0x80);
+						if($array{$j} === $commonValue and $array{$j80} === $commonValue){
+							//values are already filled
+						}else{
+							$i1 = ord($array{$j});
+							$i2 = ord($array{$j80});
+							$result{$i}        = chr(($i2 << 4) | ($i1 & 0x0f));
+							$result{$i | 0x80} = chr(($i1 >> 4) | ($i2 & 0xf0));
+						}
+						$i++;
+					}
+				}
+				$i += 128;
 			}
- 			$i += 128;
- 		}
- 		return $result;
- 	}
- 
+		}
+		return $result;
+	}
+	
  	/**
  	 * Converts pre-MCPE-1.0 biome color array to biome ID array.
  	 *
