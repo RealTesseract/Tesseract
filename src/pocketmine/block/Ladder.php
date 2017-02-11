f5780fc411393976105f842e322749453024f2e2
Apple <ytapplegamer@gmail.com>
2017-02-11 11:05:15 -0500

    Fix ladders, close #199

--- src/pocketmine/block/Ladder.php
+++ src/pocketmine/block/Ladder.php
@@ -106,18 +106,20 @@ class Ladder extends Transparent{
 	}
 
 	public function onUpdate($type){
-		if($type === Level::BLOCK_UPDATE_NORMAL){
 		$faces = [
 			2 => 3,
 			3 => 2,
 			4 => 5,
 			5 => 4,
 		];
-				if (!$this->getSide($faces[$this->meta])->isSolid()){//replace with common break method
+		if($type === Level::BLOCK_UPDATE_NORMAL){
+			if(isset($faces[$this->meta])) {
+				if ($this->getSide($faces[$this->meta])->getId() === self::AIR) {
 					$this->getLevel()->useBreakOn($this);
-				return Level::BLOCK_UPDATE_NORMAL;
 				}
+				return Level::BLOCK_UPDATE_NORMAL;
 			}
+		}
 		return false;
 	}
 	
