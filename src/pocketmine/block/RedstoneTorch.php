956cfb439908f5278d3180e358327f30f21124d2
hmy2001 <harutowii@yahoo.co.jp>
2017-02-11 10:19:59 +0900

    Update RedstoneTorch.php

--- src/pocketmine/block/RedstoneTorch.php
+++ src/pocketmine/block/RedstoneTorch.php
@@ -26,7 +26,7 @@ use pocketmine\level\Level;
 use pocketmine\Player;
 use pocketmine\math\Vector3;
 
-class RedstoneTorch extends Solid{
+class RedstoneTorch extends Flowable{
 
 	protected $id = self::REDSTONE_TORCH;
 	protected $ignore = "";
@@ -43,9 +43,40 @@ class RedstoneTorch extends Solid{
 		return "Redstone Torch";
 	}
 
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+		$below = $this->getSide(0);
+
+		if($target->isTransparent() === false and $face !== 0){
+			$faces = [
+				1 => 5,
+				2 => 4,
+				3 => 3,
+				4 => 2,
+				5 => 1,
+			];
+			$this->meta = $faces[$face];
+			$this->getLevel()->setBlock($block, $this, true, true);
+
+			return true;
+		}elseif(
+				$below->isTransparent() === false or $below->getId() === self::FENCE or
+				$below->getId() === self::COBBLE_WALL or
+				$below->getId() == Block::INACTIVE_REDSTONE_LAMP or
+				$below->getId() == Block::ACTIVE_REDSTONE_LAMP
+		){
+			$this->meta = 0;
+			$this->getLevel()->setBlock($block, $this, true, true);
+
+			return true;
+		}
+
+		return false;
+	}
+
 	public function getDrops(Item $item) : array{
 		return [
 			[Item::REDSTONE_TORCH, 0, 1],
 		];
 	}
 }
+
