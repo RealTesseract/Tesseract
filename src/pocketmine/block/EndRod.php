cf602c600ccdaaf60fe40963a5f9c020871a97bd
Apple <ytapplegamer@gmail.com>
2017-02-11 23:26:21 -0500

    Correct implementation of EndRod

--- src/pocketmine/block/EndRod.php
+++ src/pocketmine/block/EndRod.php
@@ -27,58 +27,53 @@ use pocketmine\Player;
 use pocketmine\level\Level;
 use pocketmine\item\Item;
 
-class EndRod extends Flowable{
+class EndRod extends Flowable
+{
 
 	protected $id = self::END_ROD;
 
-	public function __construct($meta = 0){
+	public function __construct($meta = 0)
+	{
 		$this->meta = $meta;
 	}
 
-	public function getLightLevel(){
+	public function getLightLevel()
+	{
 		return 14;
 	}
 
-	public function getName() : string{
+	public function getName() : string
+	{
 		return "End Rod";
 	}
 
-	public function onUpdate($type){
-		if($type === Level::BLOCK_UPDATE_NORMAL){
-			$below = $this->getSide(0);
-			$side = $this->getDamage();
+	public function getResistance()
+    {
+        return 0;
+    }
+	
+	public function getHardness()
+    {
+        return 0;
+    }
+
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null)
+	{
 			$faces = [
-				1 => 1,
-				2 => 2,
-				3 => 3,
-				4 => 4,
-				5 => 5,
-				6 => 6,
 				0 => 0,
-			];
-		}
-		return false;
-	}
-
-	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		$below = $this->getSide(0);
-
-		if($target->isTransparent() === false and $face !== 0){
-			$faces = [
 				1 => 1,
 				2 => 3,
 				3 => 2,
 				4 => 5,
 				5 => 4,
 			];
-			$this->meta = $faces[$face];
+			$this->meta = ($target->getId() === self::END_ROD && $faces[$face] == $target->getDamage()) ? Vector3::getOppositeSide($faces[$face]) : $faces[$face];
 			$this->getLevel()->setBlock($block, $this, true, true);
 			return true;
 		}
-		return false;
-	}
 
-	public function getDrops(Item $item) : array {
+	public function getDrops(Item $item) : array
+	{
 		return [
 			[$this->id, 0, 1],
 		];
