5200941df067f262d49ded866b1328da219ff7e9
Enes Yıldırım <enes5519@gmail.com>
2017-03-01 05:44:14 +0300

    Fix Perm and Others (TransferCommand)

--- src/pocketmine/command/defaults/TransferCommand.php
+++ src/pocketmine/command/defaults/TransferCommand.php
@@ -5,7 +5,6 @@ namespace pocketmine\command\defaults;
 use pocketmine\network\protocol\TransferPacket;
 use pocketmine\command\CommandSender;
 
-
 use pocketmine\Player;
 
 class TransferCommand extends VanillaCommand{
@@ -21,22 +20,23 @@ class TransferCommand extends VanillaCommand{
     }
 
     public function execute(CommandSender $sender, $currentAlias, array $args){
-
+        if(!$this->testPermission($sender)){
+            return false;
+        }
         if(!$sender instanceof Player){
-
             $sender->sendMessage("Run command in-game");
             return;
         }
-        if(!isset($args[0]) || !isset($args[1])){
-
-            $sender->sendMessage("Please provide a ip and port. /transferserver <ip> <port>");
+        if(!isset($args[0])){
+            $sender->sendMessage("Please provide a ip and port. /transferserver <ip> [port]");
             return;
         }
-
-        $sender->sendMessage("Transferring you to $args[0]".":".$args[1]);
+        $port = 19132;
+        if(!empty($args[1])){ $port=$args[1]; }
+        $sender->sendMessage("Transferring you to $args[0]".":".$port);
         $pk = new TransferPacket();
         $pk->address = $args[0];
-        $pk->port = $args[1];
+        $pk->port = $port;
         $sender->dataPacket($pk);
-}
     }
+}
