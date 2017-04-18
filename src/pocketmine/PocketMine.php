f0d12bae6fa0fa88a61913cbc7fb1b1f0e3c71de
LinuxLogo <rodjon97@outlook.com>
2017-04-18 01:30:39 -0400

    Codename Change

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -72,9 +72,9 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "1.2.0";
+	const VERSION = "1.2.1";
 	const API_VERSION = "3.0.1";
-	const CODENAME = "Ruby";
+	const CODENAME = "RottenApple";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
