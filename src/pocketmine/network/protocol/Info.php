a278ba9fa1f89d0a74bcbb8863433385f213072f
RealTesseract <realtesseract@gmail.com>
2017-06-14 09:18:48 -0700

    Updated Info.php
    Working on connection issue.

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -31,10 +31,10 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 
-	const CURRENT_PROTOCOL = 107;
-	const ACCEPTED_PROTOCOLS = [107];
-	const MINECRAFT_VERSION = "v1.0.9";
-	const MINECRAFT_VERSION_NETWORK = "1.0.9";
+	const CURRENT_PROTOCOL = 113;
+	const ACCEPTED_PROTOCOLS = [113];
+	const MINECRAFT_VERSION = "v1.1.0.55";
+	const MINECRAFT_VERSION_NETWORK = "1.1.0.55";
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
